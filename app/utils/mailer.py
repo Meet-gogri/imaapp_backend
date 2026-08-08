d643aca1aa408b41a4246259449394d76e698d41
Meet Gogri <meetgogri28@gmail.com>
2026-08-08 14:35:14 +0530

    Auto-update backend: 08-08-2026 14:35:14.40

--- app/utils/mailer.py
+++ app/utils/mailer.py
@@ -2,11 +2,11 @@ import os
 import smtplib
 from email.message import EmailMessage
 
-SMTP_HOST = os.getenv("smtppro.zoho.in")
-SMTP_PORT = int(os.getenv("465"))
-SMTP_USER = os.getenv("no_reply@policyera.com")
-SMTP_PASS = os.getenv("EvLTUpMTLWeQ")
-SMTP_FROM = os.getenv("no_reply@policyera.com")
+SMTP_HOST = "smtppro.zoho.in"
+SMTP_PORT = 465
+SMTP_USER = "no_reply@policyera.com"
+SMTP_PASS = "EvLTUpMTLWeQ"
+SMTP_FROM = "no_reply@policyera.com"
 
 
 def send_otp_email(to_email: str, code: str):
